a67afd038e073e77c365913bdeda8cafd104040b
Jan Kvetina <jan.kvetina@gmail.com>
2020-07-01 07:27:54 +0200

    Jobs with lower priority + recreate

--- jobs/bug_process_dml_errors.sql
+++ jobs/bug_process_dml_errors.sql
@@ -1,5 +1,5 @@
 DECLARE
-    in_job_name CONSTANT VARCHAR2(30) := 'BUG_PURGE_OLD';
+    in_job_name CONSTANT VARCHAR2(30) := 'BUG_PROCESS_DML_ERRORS';
 BEGIN
     BEGIN
         DBMS_SCHEDULER.DROP_JOB(in_job_name, TRUE);  -- force
@@ -11,11 +11,11 @@ BEGIN
     DBMS_SCHEDULER.CREATE_JOB (
         job_name            => in_job_name,
         job_type            => 'STORED_PROCEDURE',
-        job_action          => 'bug.purge_old',
+        job_action          => 'bug.process_dml_errors',
         start_date          => SYSDATE,
-        repeat_interval     => 'FREQ=DAILY; BYHOUR=2; BYMINUTE=0',  -- 02:00
+        repeat_interval     => 'FREQ=MINUTELY;INTERVAL=1;',
         enabled             => FALSE,
-        comments            => 'Purge old records from debug_log table'
+        comments            => 'Merge DML ERR records into proper debug_log tree'
     );
     --
     DBMS_SCHEDULER.SET_ATTRIBUTE(in_job_name, 'JOB_PRIORITY', 5);  -- lower priority
