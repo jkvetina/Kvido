19a515d582c2b66f96cf72496953083b201dd56a
Jan Kvetina <jan.kvetina@gmail.com>
2020-07-04 10:17:14 +0200

    Renamed

--- jobs/bug_process_dml.sql
+++ jobs/bug_process_dml.sql
@@ -1,5 +1,6 @@
+@@ -0,0 +1,36 @@
 DECLARE
-    in_job_name CONSTANT VARCHAR2(30) := 'BUG_PROCESS_DML_ERRORS';
+    in_job_name CONSTANT VARCHAR2(30) := 'BUG_PROCESS_DML';
 BEGIN
     BEGIN
         DBMS_SCHEDULER.DROP_JOB(in_job_name, TRUE);  -- force
@@ -11,9 +12,9 @@ BEGIN
     DBMS_SCHEDULER.CREATE_JOB (
         job_name            => in_job_name,
         job_type            => 'STORED_PROCEDURE',
-        job_action          => 'bug.process_dml_errors',
+        job_action          => 'bug_process_dml_errors',    -- procedure, not package
         start_date          => SYSDATE,
-        repeat_interval     => 'FREQ=MINUTELY;INTERVAL=1;',
+        repeat_interval     => 'FREQ=MINUTELY;INTERVAL=5;',
         enabled             => FALSE,
         comments            => 'Merge DML ERR records into proper debug_log tree'
     );
@@ -25,11 +26,11 @@ END;
 /
 
 BEGIN
-    DBMS_SCHEDULER.RUN_JOB('BUG_PURGE_OLD');
+    DBMS_SCHEDULER.RUN_JOB('BUG_PROCESS_DML');
     COMMIT;
 END;
 /
+
 /*
 SELECT job_name, run_count, failure_count FROM user_scheduler_jobs j ORDER BY 1;
 */
-
