20dd1a19c55f93160ba81c288fd7c125dfc21849
Jan Kvetina <jan.kvetina@gmail.com>
2020-07-06 21:01:17 +0200

    Fixed specifications

--- views/debug_log_modules.sql
+++ views/debug_log_modules.sql
@@ -10,11 +10,12 @@ WITH p AS (
         LEAD(i.line) OVER (PARTITION BY i.object_name, i.object_type ORDER BY i.line) - 1       AS end_line
     FROM user_identifiers i
     JOIN user_source s
-        ON s.name       = i.object_name
-        AND s.type      = i.object_type
-        AND s.line      = i.line
-    WHERE i.type        IN ('PROCEDURE', 'FUNCTION')
-        AND i.usage     = CASE s.type WHEN 'PACKAGE BODY' THEN 'DEFINITION' ELSE 'DECLARATION' END
+        ON s.name           = i.object_name
+        AND s.type          = i.object_type
+        AND s.line          = i.line
+    WHERE i.type            IN ('PROCEDURE', 'FUNCTION')
+        AND i.object_type   IN ('PACKAGE', 'PACKAGE BODY')
+        AND i.usage         = CASE s.type WHEN 'PACKAGE BODY' THEN 'DEFINITION' ELSE 'DECLARATION' END
 )
 SELECT
     p.object_name,
@@ -23,20 +24,26 @@ SELECT
     p.type,
     p.overload,
     p.start_line,
-    MAX(s.line)                 AS end_line,
-    MAX(s.line) - p.start_line  AS lines
+    MAX(s.line)                     AS end_line,
+    MAX(s.line) - p.start_line + 1  AS lines
 FROM p
 LEFT JOIN (
-    SELECT s.name, MAX(s.line) - 1 AS last_line
+    SELECT s.name, s.type, MAX(s.line) - 1 AS last_line
     FROM user_source s
-    WHERE s.text LIKE 'BEGIN%'  -- package block
-    GROUP BY s.name
+    WHERE s.text        LIKE 'BEGIN%'  -- package block
+        AND s.type      IN ('PACKAGE', 'PACKAGE BODY')
+    GROUP BY s.name, s.type
 ) b
-    ON b.name = p.object_name
+    ON b.name   = p.object_name
+    AND b.type  = p.object_type
 LEFT JOIN user_source s
-    ON s.line   > p.start_line
-    AND s.line  <= COALESCE(p.end_line, b.last_line, 999999)
-    AND REGEXP_LIKE(UPPER(s.text), '^\s*END(\s+' || p.name || ')?\s*;', 'i')  -- find module end
+    ON s.name   = p.object_name
+    AND s.type  = p.object_type
+    AND s.line  BETWEEN p.start_line AND COALESCE(p.end_line, b.last_line, 999999)
+    AND (
+        (s.type = 'PACKAGE BODY' AND REGEXP_LIKE(UPPER(s.text), '^\s*END(\s+' || p.name || ')?\s*;')) OR
+        (s.type = 'PACKAGE'      AND REGEXP_LIKE(UPPER(s.text), ';'))
+    )
 GROUP BY p.object_name, p.object_type, p.name, p.type, p.overload, p.start_line, p.end_line
 ORDER BY p.object_name, p.object_type, p.start_line;
 --
