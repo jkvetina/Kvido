ddf5e5da3dfb334f1c29b34f4a1d965a3a6cbb5e
Jan Kvetina <jan.kvetina@gmail.com>
2020-07-07 08:33:43 +0200

    Pivoted spec and body

--- views/debug_log_modules.sql
+++ views/debug_log_modules.sql
@@ -1,10 +1,21 @@
 CREATE OR REPLACE VIEW debug_log_modules AS
-WITH p AS (
+SELECT
+    p.object_name AS package_name,
+    p.module_name,
+    p.module_type,
+    p.overload,
+    MAX(CASE p.object_type WHEN 'PACKAGE'       THEN p.start_line END)              AS spec_start,
+    MAX(CASE p.object_type WHEN 'PACKAGE'       THEN s.line END)                    AS spec_end,
+    MAX(CASE p.object_type WHEN 'PACKAGE'       THEN s.line - p.start_line + 1 END) AS spec_lines,
+    MAX(CASE p.object_type WHEN 'PACKAGE BODY'  THEN p.start_line END)              AS body_start,
+    MAX(CASE p.object_type WHEN 'PACKAGE BODY'  THEN s.line END)                    AS body_end,
+    MAX(CASE p.object_type WHEN 'PACKAGE BODY'  THEN s.line - p.start_line + 1 END) AS body_lines
+FROM (
     SELECT
         i.object_name,
         i.object_type,
-        i.name,
-        i.type,
+        i.name          AS module_name,
+        i.type          AS module_type,
         ROW_NUMBER() OVER (PARTITION BY i.object_name, i.object_type, i.name ORDER BY i.line)   AS overload,
         i.line                                                                                  AS start_line,
         LEAD(i.line) OVER (PARTITION BY i.object_name, i.object_type ORDER BY i.line) - 1       AS end_line
@@ -16,17 +27,7 @@ WITH p AS (
     WHERE i.type            IN ('PROCEDURE', 'FUNCTION')
         AND i.object_type   IN ('PACKAGE', 'PACKAGE BODY')
         AND i.usage         = CASE s.type WHEN 'PACKAGE BODY' THEN 'DEFINITION' ELSE 'DECLARATION' END
-)
-SELECT
-    p.object_name,
-    p.object_type,
-    p.name,
-    p.type,
-    p.overload,
-    p.start_line,
-    MAX(s.line)                     AS end_line,
-    MAX(s.line) - p.start_line + 1  AS lines
-FROM p
+) p
 LEFT JOIN (
     SELECT s.name, s.type, MAX(s.line) - 1 AS last_line
     FROM user_source s
@@ -41,20 +42,22 @@ LEFT JOIN user_source s
     AND s.type  = p.object_type
     AND s.line  BETWEEN p.start_line AND COALESCE(p.end_line, b.last_line, 999999)
     AND (
-        (s.type = 'PACKAGE BODY' AND REGEXP_LIKE(UPPER(s.text), '^\s*END(\s+' || p.name || ')?\s*;')) OR
+        (s.type = 'PACKAGE BODY' AND REGEXP_LIKE(UPPER(s.text), '^\s*END(\s+' || p.module_name || ')?\s*;')) OR
         (s.type = 'PACKAGE'      AND REGEXP_LIKE(UPPER(s.text), ';'))
     )
-GROUP BY p.object_name, p.object_type, p.name, p.type, p.overload, p.start_line, p.end_line
-ORDER BY p.object_name, p.object_type, p.start_line;
+GROUP BY p.object_name, p.module_name, p.module_type, p.overload
+ORDER BY p.object_name, MIN(p.start_line);
 --
 COMMENT ON TABLE debug_log_modules IS 'Find package modules (procedures and functions) and their boundaries (start-end lines)';
 --
-COMMENT ON COLUMN debug_log_modules.object_name IS 'Object name';
-COMMENT ON COLUMN debug_log_modules.object_type IS 'Object type';
-COMMENT ON COLUMN debug_log_modules.name        IS 'Module name';
-COMMENT ON COLUMN debug_log_modules.type        IS 'Module type';
-COMMENT ON COLUMN debug_log_modules.overload    IS 'Overload ID';
-COMMENT ON COLUMN debug_log_modules.start_line  IS 'Module start';
-COMMENT ON COLUMN debug_log_modules.end_line    IS 'Module end';
-COMMENT ON COLUMN debug_log_modules.lines       IS 'Lines';
+COMMENT ON COLUMN debug_log_modules.package_name    IS 'Package name';
+COMMENT ON COLUMN debug_log_modules.module_name     IS 'Module name';
+COMMENT ON COLUMN debug_log_modules.module_type     IS 'Module type';
+COMMENT ON COLUMN debug_log_modules.overload        IS 'Overload ID';
+COMMENT ON COLUMN debug_log_modules.spec_start      IS 'Module start in specification';
+COMMENT ON COLUMN debug_log_modules.spec_end        IS 'Module end in specification';
+COMMENT ON COLUMN debug_log_modules.spec_lines      IS 'Lines in specification';
+COMMENT ON COLUMN debug_log_modules.body_start      IS 'Module start in body';
+COMMENT ON COLUMN debug_log_modules.body_end        IS 'Module end in body';
+COMMENT ON COLUMN debug_log_modules.body_lines      IS 'Lines in body';
 
--- views/debug_log_profiler_sum.sql
+++ views/debug_log_profiler_sum.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW debug_log_profiler_sum AS
 SELECT
-    p.name,
+    p.package_name,
     p.module_name,
     p.module_type,
     p.overload,
@@ -8,24 +8,23 @@ SELECT
     ROUND(p.total_time / SUM(p.total_time) OVER() * 100, 2) AS module_perc
 FROM (
     SELECT
-        p.name,
-        m.name              AS module_name,
-        m.type              AS module_type,
+        p.name              AS package_name,
+        m.module_name,
+        m.module_type,
         m.overload,
         SUM(p.total_time)   AS total_time
     FROM debug_log_profiler p
     LEFT JOIN debug_log_modules m
-        ON m.object_name    = p.name
-        AND m.object_type   = p.type
-        AND p.line          BETWEEN m.start_line AND m.end_line
+        ON m.package_name   = p.name
+        AND p.line          BETWEEN m.body_start AND m.body_end
     WHERE p.total_time      > 0
-    GROUP BY p.name, m.name, m.type, m.overload
+    GROUP BY p.name, m.module_name, m.module_type, m.overload
 ) p
-ORDER BY total_time DESC, p.name, p.module_name, p.overload;
+ORDER BY total_time DESC, p.package_name, p.module_name, p.overload;
 --
-COMMENT ON COLUMN debug_log_profiler_sum.name           IS 'Object/package name';
-COMMENT ON COLUMN debug_log_profiler_sum.module_name    IS 'Name of module (function/procedure)';
-COMMENT ON COLUMN debug_log_profiler_sum.module_type    IS 'Module type';
+COMMENT ON COLUMN debug_log_profiler_sum.package_name   IS 'Package name';
+COMMENT ON COLUMN debug_log_profiler_sum.module_name    IS 'Module name';
+COMMENT ON COLUMN debug_log_profiler_sum.module_type    IS 'Module type (function/procedure)';
 COMMENT ON COLUMN debug_log_profiler_sum.overload       IS 'Overload ID';
 COMMENT ON COLUMN debug_log_profiler_sum.total_time     IS 'Time spent on module';
 COMMENT ON COLUMN debug_log_profiler_sum.module_perc    IS 'Percentage of total time spent on module';
