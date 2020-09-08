700082bbf144e6f86092be2c1cb5c4ddefd3cfc8
Jan Kvetina <jan.kvetina@gmail.com>
2020-09-08 20:53:05 +0200

    Reuse settings for NULLs

--- procedures/recompile.sql
+++ procedures/recompile.sql
@@ -4,10 +4,14 @@ CREATE OR REPLACE PROCEDURE recompile (
     in_code_type        VARCHAR2    := 'INTERPRETED',
     in_scope            VARCHAR2    := 'IDENTIFIERS:ALL, STATEMENTS:ALL',
     in_warnings         VARCHAR2    := 'ENABLE:SEVERE, ENABLE:PERFORMANCE',
-    in_optimize         NUMBER      := 3,
+    in_optimize         NUMBER      := 2,
     in_ccflags          VARCHAR2    := NULL,
-    in_invalid_only     CHAR        := 'Y'
+    in_invalid_only     BOOLEAN     := TRUE
 ) AS
+    in_invalids         CONSTANT CHAR       := CASE WHEN in_invalid_only THEN 'Y' END;
+    ccflags             VARCHAR2(32767);
+    query_              VARCHAR2(32767);
+    invalids            PLS_INTEGER;
 BEGIN
     /**
      * This procedure is part of the BUG project under MIT licence.
@@ -21,53 +25,56 @@ BEGIN
     DBMS_OUTPUT.PUT('INVALID: ');
     --
     FOR c IN (
-        SELECT o.object_name, o.object_type, '' AS ccflags
+        SELECT o.object_name, o.object_type
         FROM user_objects o
         WHERE o.status          != 'VALID'
             AND o.object_type   NOT IN ('SEQUENCE', 'MATERIALIZED VIEW')
             AND o.object_name   != $$PLSQL_UNIT         -- not this procedure
         UNION ALL
-        SELECT o.object_name, o.object_type, '' AS ccflags
+        SELECT o.object_name, o.object_type
         FROM user_objects o
-        WHERE in_invalid_only   != 'Y'
+        WHERE in_invalids       IS NULL
             AND o.object_type   IN ('PACKAGE', 'PACKAGE BODY', 'PROCEDURE', 'FUNCTION', 'TRIGGER', 'VIEW', 'SYNONYM')
             AND o.object_type   LIKE in_filter_type
             AND o.object_name   != $$PLSQL_UNIT         -- not this procedure
             AND o.object_name   LIKE in_filter_name
     ) LOOP
         -- apply ccflags only relevant to current object
-        BEGIN
-            SELECT
-                LISTAGG(REGEXP_SUBSTR(in_ccflags, '(' || s.flag_name || ':[^,]+)', 1, 1, NULL, 1), ', ')
-                    WITHIN GROUP (ORDER BY s.flag_name)
-            INTO c.ccflags
-            FROM (
-                SELECT DISTINCT REGEXP_SUBSTR(s.text, '[$].*\s[$][$]([A-Z0-9-_]+)\s.*[$]', 1, 1, NULL, 1) AS flag_name
-                FROM user_source s
-                WHERE REGEXP_LIKE(s.text, '[$].*\s[$][$][A-Z0-9-_]+\s.*[$]')
-                    AND s.name = c.object_name
-                    AND s.type = c.object_type
-            ) s;
-        EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            c.ccflags := '';
-        END;
+        IF in_ccflags IS NOT NULL THEN
+            BEGIN
+                SELECT
+                    LISTAGG(REGEXP_SUBSTR(in_ccflags, '(' || s.flag_name || ':[^,]+)', 1, 1, NULL, 1), ', ')
+                        WITHIN GROUP (ORDER BY s.flag_name)
+                INTO ccflags
+                FROM (
+                    SELECT DISTINCT REGEXP_SUBSTR(s.text, '[$].*\s[$][$]([A-Z0-9-_]+)\s.*[$]', 1, 1, NULL, 1) AS flag_name
+                    FROM user_source s
+                    WHERE REGEXP_LIKE(s.text, '[$].*\s[$][$][A-Z0-9-_]+\s.*[$]')
+                        AND s.name = c.object_name
+                        AND s.type = c.object_type
+                ) s;
+            EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                ccflags := NULL;
+            END;
+        END IF;
         --
         BEGIN
-            EXECUTE IMMEDIATE
-                'ALTER ' || REPLACE(c.object_type, ' BODY', '') || ' ' ||
-                c.object_name || ' COMPILE' ||
+            query_ := 'ALTER ' || REPLACE(c.object_type, ' BODY', '') || ' ' || c.object_name || ' COMPILE' ||
                 CASE WHEN c.object_type LIKE '% BODY' THEN ' BODY' END || ' ' ||
                 CASE WHEN c.object_type IN (
                     'PACKAGE', 'PACKAGE BODY', 'PROCEDURE', 'FUNCTION', 'TRIGGER'
                 ) THEN
-                    'PLSQL_CCFLAGS = '''        || RTRIM(c.ccflags) || ''' ' ||
-                    'PLSQL_CODE_TYPE = '        || in_code_type || ' ' ||
-                    'PLSQL_OPTIMIZE_LEVEL = '   || in_optimize || ' ' ||
-                    'PLSQL_WARNINGS = '''       || REPLACE(in_warnings, ',', ''', ''') || ''' ' ||
-                    'PLSCOPE_SETTINGS = '''     || in_scope || ''' ' ||
-                    'REUSE SETTINGS'            -- store in user_plsql_object_settings
+                    CASE WHEN ccflags       IS NOT NULL THEN 'PLSQL_CCFLAGS = '''       || RTRIM(ccflags) || ''' ' END ||
+                    CASE WHEN in_code_type  IS NOT NULL THEN 'PLSQL_CODE_TYPE = '       || in_code_type || ' ' END ||
+                    CASE WHEN in_optimize   IS NOT NULL THEN 'PLSQL_OPTIMIZE_LEVEL = '  || in_optimize || ' ' END ||
+                    CASE WHEN in_warnings   IS NOT NULL THEN 'PLSQL_WARNINGS = '''      || REPLACE(in_warnings, ',', ''', ''') || ''' ' END ||
+                    CASE WHEN in_scope      IS NOT NULL THEN 'PLSCOPE_SETTINGS = '''    || RTRIM(in_scope) || ''' ' END ||
+                    'REUSE SETTINGS'
                 END;
+            --
+            EXECUTE IMMEDIATE query_;
+            --DBMS_OUTPUT.PUT_LINE(query_);
             DBMS_OUTPUT.PUT('.');
         EXCEPTION
         WHEN OTHERS THEN
@@ -76,33 +83,33 @@ BEGIN
     END LOOP;
 
     -- show number of invalid objects
-    FOR c IN (
-        SELECT COUNT(*) AS count_
-        FROM user_objects o
-        WHERE o.status          != 'VALID'
-            AND o.object_type   != 'MATERIALIZED VIEW'
-            AND o.object_name   != $$PLSQL_UNIT         -- not this procedure
-    ) LOOP
-        DBMS_OUTPUT.PUT_LINE(' -> ' || c.count_);
-    END LOOP;
+    SELECT COUNT(*) INTO invalids
+    FROM user_objects o
+    WHERE o.status          != 'VALID'
+        AND o.object_type   != 'MATERIALIZED VIEW'
+        AND o.object_name   != $$PLSQL_UNIT;        -- not this procedure
+    --
+    DBMS_OUTPUT.PUT_LINE(' -> ' || invalids);
+    DBMS_OUTPUT.PUT_LINE('');
 
     -- list invalid objects
-    DBMS_OUTPUT.PUT_LINE('');
-    FOR c IN (
-        SELECT object_type, LISTAGG(object_name, ', ') WITHIN GROUP (ORDER BY object_name) AS objects
-        FROM (
-            SELECT DISTINCT o.object_type, o.object_name
-            FROM user_objects o
-            WHERE o.status      != 'VALID'
-            AND o.object_type   != 'MATERIALIZED VIEW'
-            ORDER BY o.object_type, o.object_name
-        )
-        GROUP BY object_type
-        ORDER BY object_type
-    ) LOOP
-        DBMS_OUTPUT.PUT_LINE('  ' || RPAD(c.object_type || ':', 15, ' ') || ' ' || c.objects);
-    END LOOP;
-    DBMS_OUTPUT.PUT_LINE('');
+    IF invalids > 0 THEN
+        FOR c IN (
+            SELECT object_type, LISTAGG(object_name, ', ') WITHIN GROUP (ORDER BY object_name) AS objects
+            FROM (
+                SELECT DISTINCT o.object_type, o.object_name
+                FROM user_objects o
+                WHERE o.status      != 'VALID'
+                AND o.object_type   != 'MATERIALIZED VIEW'
+                ORDER BY o.object_type, o.object_name
+            )
+            GROUP BY object_type
+            ORDER BY object_type
+        ) LOOP
+            DBMS_OUTPUT.PUT_LINE('  ' || RPAD(c.object_type || ':', 15, ' ') || ' ' || c.objects);
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('');
+    END IF;
 END;
 /
 
