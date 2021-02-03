bf58d4862364719ec1977ad679d302a12b9d122e
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-03 20:39:48 +0100

    Fixed triggers

--- triggers/navigation_def.sql
+++ triggers/navigation_def.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE TRIGGER navigation_def
 BEFORE INSERT OR UPDATE ON navigation
 FOR EACH ROW
 BEGIN
-    :NEW.app_id         := COALESCE(sess.get_app_id(), :NEW.app_id);
+    :NEW.app_id         := COALESCE(NULLIF(sess.get_app_id(), 0), :NEW.app_id);
 END;
 /
--- triggers/navigation_groups_def.sql
+++ triggers/navigation_groups_def.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE TRIGGER navigation_groups_def
 BEFORE INSERT OR UPDATE ON navigation_groups
 FOR EACH ROW
 BEGIN
-    :NEW.app_id         := COALESCE(sess.get_app_id(), :NEW.app_id);
+    :NEW.app_id         := COALESCE(NULLIF(sess.get_app_id(), 0), :NEW.app_id);
 END;
 /
