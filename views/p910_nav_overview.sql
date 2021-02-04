6b5ca4840d803977a82b1b68377af57054438dee
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-04 20:34:44 +0100

    Improved main view for page 910, added links to open APEX Developer

--- views/p910_nav_overview.sql
+++ views/p910_nav_overview.sql
@@ -4,21 +4,31 @@ SELECT
     n.page_id,
     n.parent_id,
     p.page_alias,
+    p.page_name,
     --
-    CASE WHEN r.page_id IS NOT NULL
-        THEN '<span class="fa fa-minus-square" style="color: #666;" title=""></span>'
+    CASE
+        WHEN r.page_id IS NOT NULL
+            THEN '<span class="fa fa-minus-square" style="color: #666;" title="Remove record from Navigation table"></span>'
         END AS status,
-        --
-        -- fa-check-square FOR UPDATED ROWS
-        --
     --
     n.order#,
-    n.label,
-    n.icon_name,
     n.css_class,
     n.is_hidden,
     p.page_group,
-    p.page_id                   AS page_link,
+    --
+    CASE WHEN p.page_id IS NOT NULL
+        THEN '<a href="' ||
+            APEX_PAGE.GET_URL (
+                p_application   => '4000',
+                p_page          => '4500',
+                p_session       => apex.get_developer_session_id(),
+                p_clear_cache   => '1,4150',
+                p_items         => 'FB_FLOW_ID,FB_FLOW_PAGE_ID,F4000_P1_FLOW,F4000_P4150_GOTO_PAGE,F4000_P1_PAGE',
+                p_values        => n.app_id || ',' || n.page_id || ',' || n.app_id || ',' || n.page_id || ',' || n.page_id
+            ) ||
+            '"><span class="fa fa-external-link" style="color: #333;" title="Open page in APEX"></span></a>'
+        END AS page_link,
+    --
     p.authorization_scheme      AS auth_scheme
 FROM navigation n
 LEFT JOIN apex_application_pages p
@@ -29,26 +39,34 @@ LEFT JOIN p910_nav_pages_to_remove r
     AND r.page_id               = n.page_id
 WHERE n.app_id                  = sess.get_app_id()
     AND n.page_id               BETWEEN 1 AND 999  -- sess.app_min_page AND sess.app_max_page
-    --
-    AND NVL(p.page_group, '-')              = COALESCE(apex.get_item('$PAGE_GROUP'),  p.page_group, '-')
-    AND NVL(p.authorization_scheme, '-')    = COALESCE(apex.get_item('$AUTH_SCHEME'), p.authorization_scheme, '-')
 UNION ALL
 SELECT
     a.app_id,
     a.page_id,
     a.parent_id,
     a.page_alias,
+    a.page_name,
     --
-    '<span class="fa fa-plus-square" style="color: #666;" title=""></span>' AS status,
+    '<span class="fa fa-plus-square" style="color: #666;" title="Create record in Navigation table"></span>' AS status,
     --
     a.order#,
-    a.label,
-    a.icon_name,
     a.css_class,
     a.is_hidden,
     a.page_group,
-    a.page_link,
+    --
+    '<a href="' ||
+    APEX_PAGE.GET_URL (
+        p_application   => '4000',
+        p_page          => '4500',
+        p_session       => apex.get_developer_session_id(),
+        p_clear_cache   => '1,4150',
+        p_items         => 'FB_FLOW_ID,FB_FLOW_PAGE_ID,F4000_P1_FLOW,F4000_P4150_GOTO_PAGE,F4000_P1_PAGE',
+        p_values        => a.app_id || ',' || a.page_id || ',' || a.app_id || ',' || a.page_id || ',' || a.page_id
+    ) ||
+    '"><span class="fa fa-external-link" style="color: #333;" title="Open page in APEX"></span></a>' AS page_link,
+    --
     a.auth_scheme
 FROM p910_nav_pages_to_add a
-
-ORDER BY 1, 2;
+WHERE a.app_id          = sess.get_app_id()
+    AND a.page_id       BETWEEN 1 AND 999   -- sess.app_min_page AND sess.app_max_page
+;
