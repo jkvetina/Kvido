7381e2475fee30842dea5d87c8979e017e439daa
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-04 20:32:25 +0100

    Improved Nav and Auth Groups

--- views/p910_auth_groups.sql
+++ views/p910_auth_groups.sql
@@ -3,6 +3,7 @@ SELECT
     a.authorization_scheme_name     AS auth_scheme,
     MAX(s.procedure_name)           AS auth_procedure,
     MAX(a.attribute_01)             AS auth_source,
+    MAX(p.page_group)               AS page_group,
     MAX(a.error_message)            AS error_message,
     MAX(a.caching)                  AS caching,
     --
@@ -10,7 +11,8 @@ SELECT
         THEN '<span class="fa fa-check-square" style="color: #666;" title="Auth procedure exists"></span>'
         END AS status,
     --
-    NULLIF(COUNT(p.page_id), 0)     AS pages
+    NULLIF(COUNT(p.page_id), 0)     AS pages,
+    NULLIF(MAX(u.users), 0)         AS users
 FROM apex_application_authorization a
 LEFT JOIN apex_application_pages p
     ON p.application_id             = a.application_id
@@ -18,7 +20,13 @@ LEFT JOIN apex_application_pages p
 LEFT JOIN user_procedures s
     ON s.object_name                = 'AUTH'  -- nav.auth_package
     AND s.procedure_name            = p.authorization_scheme
+LEFT JOIN (
+    SELECT
+        'IS_' || u.role_id  AS role_id,
+        COUNT(u.role_id)    AS users
+    FROM user_roles u
+    GROUP BY u.role_id
+) u
+    ON u.role_id                    = a.authorization_scheme_name
 WHERE a.application_id              = sess.get_app_id()
-    AND p.page_group                = NVL(apex.get_item('$PAGE_GROUP'), p.page_group)
-    AND a.authorization_scheme_name = NVL(apex.get_item('$AUTH_SCHEME'), a.authorization_scheme_name)
 GROUP BY a.authorization_scheme_name;
--- views/p910_nav_groups.sql
+++ views/p910_nav_groups.sql
@@ -1,12 +1,24 @@
 CREATE OR REPLACE VIEW p910_nav_groups AS
 SELECT
+    g.app_id,
     g.page_id,
     g.page_group,
-    COUNT(p.page_id)            AS pages
+    --
+    CASE WHEN MAX(s.procedure_name) IS NOT NULL
+        THEN '<span class="fa fa-check-square" style="color: #666;" title="Auth procedure exists"></span>'
+        END AS status,
+    --
+    NULLIF(COUNT(p.page_id), 0)         AS pages,
+    MAX(a.authorization_scheme_name)    AS auth_scheme
 FROM navigation_groups g
 LEFT JOIN apex_application_pages p
-    ON p.application_id         = g.app_id
-    AND p.page_group            = g.page_group
-WHERE g.app_id                  = sess.get_app_id()
-    AND g.page_group            = NVL(apex.get_item('$PAGE_GROUP'), g.page_group)
-GROUP BY g.page_id, g.page_group;
+    ON p.application_id                 = g.app_id
+    AND p.page_group                    = g.page_group
+LEFT JOIN apex_application_authorization a
+    ON a.application_id                 = p.application_id
+    AND a.authorization_scheme_name     = p.authorization_scheme
+LEFT JOIN user_procedures s
+    ON s.object_name                    = 'AUTH'  -- nav.auth_package
+    AND s.procedure_name                = p.authorization_scheme
+WHERE g.app_id                          = sess.get_app_id()
+GROUP BY g.app_id, g.page_id, g.page_group;
