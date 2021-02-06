94e7b62301aed28c4e26744f209d7d67fa214efa
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-06 16:10:54 +0100

    Views for APEX items peeking (at footer on page zero)

---
+++ views/apex_app_items.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE VIEW apex_app_items AS
+SELECT
+    i.item_name,
+    apex.get_item(i.item_name) AS item_value
+FROM apex_application_items i
+WHERE i.application_id  = sess.get_app_id()
+    AND i.page_id       = sess.get_page_id()
+ORDER BY 1;
