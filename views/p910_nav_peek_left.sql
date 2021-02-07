3e478f5381fc6d833df20593e137400c4a5b5ee4
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-07 13:17:24 +0100

    Move queries from APEX to views

--- views/p910_nav_peek_left.sql
+++ views/p910_nav_peek_left.sql
@@ -1,12 +1,30 @@
-CREATE OR REPLACE VIEW p910_nav_left AS
-SELECT n.page_id
-FROM navigation n
-WHERE n.app_id              = sess.get_app_id()
-    AND n.order#            < (
-        SELECT n.order#
-        FROM navigation n
-        WHERE n.app_id      = sess.get_app_id()
-            AND n.page_id   = 0
-    )
-    AND n.parent_id         IS NULL
-    AND n.page_id           > 0;
+CREATE OR REPLACE VIEW p910_nav_peek_left AS
+SELECT
+    CASE WHEN n.parent_id IS NOT NULL
+        THEN '&' || 'nbsp; ' || '&' || 'nbsp; ' || '&' || 'nbsp; '
+        END ||
+    CASE WHEN n.page_id = apex.get_item('P910_PEEK_PAGE')  -- nav.*
+        THEN '<b>' || nav.get_page_label(n.page_name) || '</b>'
+        ELSE nav.get_page_label(n.page_name)
+        END AS page_name__
+FROM nav_top_src n
+JOIN (
+    SELECT n.page_id
+    FROM navigation n
+    WHERE n.app_id              = sess.get_app_id()
+        AND n.order#            < (
+            SELECT n.order#
+            FROM navigation n
+            WHERE n.app_id      = sess.get_app_id()
+                AND n.page_id   = 0
+        )
+        AND n.parent_id         IS NULL
+        AND n.page_id           > 0
+) l
+    ON l.page_id = NVL(n.parent_id, n.page_id)
+--
+CONNECT BY n.app_id         = PRIOR n.app_id
+    AND n.parent_id         = PRIOR n.page_id
+START WITH n.parent_id      IS NULL
+ORDER SIBLINGS BY n.order# NULLS LAST, n.page_id;
+
---
+++ views/p910_nav_peek_schemes.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE VIEW p910_nav_peek_schemes AS
+SELECT
+    g.auth_scheme,
+    CASE WHEN nav.is_role_peeking_enabled(g.auth_scheme) = 'Y'
+        THEN '<b>' || g.auth_scheme || '</b>'
+        ELSE g.auth_scheme
+        END ||
+    CASE WHEN r.role_name IS NOT NULL
+        THEN ' <span class="fa fa-check-square-o" title="Role is active"></span>'
+        END AS name
+FROM p910_auth_schemes g
+LEFT JOIN user_roles_assigned r
+    ON REPLACE('IS_' || r.role_name, 'IS_IS_', 'IS_') = g.auth_scheme
+WHERE g.auth_procedure IS NOT NULL;
+
