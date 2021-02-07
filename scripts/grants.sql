ff3d53581f7c6ee5d127c454d4ae6713e2f4fed7
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-07 10:22:17 +0100

    User and grants

--- scripts/grants.sql
+++ scripts/grants.sql
@@ -1,2 +1,20 @@
-GRANT SELECT ON v$session TO dev;
+GRANT CONNECT, ALTER SESSION            TO dev;
+GRANT CREATE TABLE                      TO dev;
+GRANT CREATE VIEW                       TO dev;
+GRANT CREATE PROCEDURE                  TO dev;
+GRANT CREATE SEQUENCE                   TO dev;
+GRANT CREATE MATERIALIZED VIEW          TO dev;
+GRANT CREATE SYNONYM                    TO dev;
+GRANT CREATE TRIGGER                    TO dev;
+GRANT CREATE TYPE                       TO dev;
+--
+GRANT EXECUTE ON DBMS_SESSION           TO dev;
+GRANT EXECUTE ON DBMS_SCHEDULER         TO dev;
+GRANT EXECUTE ON DBMS_PROFILER          TO dev;
+GRANT EXECUTE ON DBMS_HPROF             TO dev;
+--
+GRANT SELECT ON v$sql                   TO dev;
+GRANT SELECT ON v$sql_cursor            TO dev;
+GRANT SELECT ON v$session               TO dev;
+GRANT SELECT ON v$session_longops       TO dev;
 
