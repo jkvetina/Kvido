d131d0261112db8ad46b3797ec6c0fd72fd46773
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-07 13:20:48 +0100

    Missing view, this should be customized with app specific roles

---
+++ views/user_roles_assigned.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW user_roles_assigned AS
+SELECT
+    t.role_name AS role_name
+FROM (
+    SELECT
+        'IS_DEVELOPER'          AS role_name,
+        auth.is_developer()     AS assigned
+    FROM DUAL
+    --
+    --UNION ALL  -- more roles
+    --
+) t
+WHERE t.assigned = 'Y';
+
