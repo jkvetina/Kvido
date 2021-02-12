a7bba58d5d84a1aa9c50ccec3a6353d8f1309d69
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-12 20:53:39 +0100

    Uploaded file details

---
+++ tables/uploaded_file_cols.sql
@@ -0,0 +1,21 @@
+--DROP TABLE uploaded_file_cols;
+CREATE TABLE uploaded_file_cols (
+    file_name           VARCHAR2(255)   NOT NULL,
+    sheet_id            NUMBER(4)       NOT NULL,
+    column_id           NUMBER(4)       NOT NULL,
+    --
+    column_name         VARCHAR2(64),
+    data_type           VARCHAR2(30),
+    format_mask         VARCHAR2(64),
+    --
+    --updated_by          VARCHAR2(30),
+    --updated_at          DATE,
+    --
+    CONSTRAINT pk_uploaded_file_cols
+        PRIMARY KEY (file_name, sheet_id, column_id),
+    --
+    CONSTRAINT fk_uploaded_file_cols_sheet_id
+        FOREIGN KEY (file_name, sheet_id)
+        REFERENCES uploaded_file_sheets (file_name, sheet_id)
+);
+
---
+++ tables/uploaded_file_sheets.sql
@@ -0,0 +1,43 @@
+--DROP TABLE uploaded_file_sheets;
+CREATE TABLE uploaded_file_sheets (
+    file_name           VARCHAR2(255)   NOT NULL,
+    sheet_id            NUMBER(4)       NOT NULL,
+    --
+    sheet_xml_id        VARCHAR2(256)   NOT NULL,
+    sheet_name          VARCHAR2(256),
+    sheet_cols          NUMBER(4),
+    sheet_rows          NUMBER(8),
+    --
+    app_id              NUMBER(4)       NOT NULL,
+    uploader_id         VARCHAR2(30),
+    --
+    profile_json        CLOB,
+    --
+    result_inserted     NUMBER(8),
+    result_updated      NUMBER(8),
+    result_deleted      NUMBER(8),
+    result_errors       NUMBER(8),
+    result_unmatched    NUMBER(8),
+    --
+    updated_by          VARCHAR2(30),
+    updated_at          DATE,
+    --
+    commited_by         VARCHAR2(30),
+    commited_at         DATE,
+    --
+    CONSTRAINT pk_uploaded_file_sheets
+        PRIMARY KEY (file_name, sheet_id),
+    --
+    CONSTRAINT fk_uploaded_file_sheets_file_name
+        FOREIGN KEY (file_name)
+        REFERENCES uploaded_files (file_name),
+    --
+    CONSTRAINT fk_uploaded_file_sheets_uploader_id
+        FOREIGN KEY (app_id, uploader_id)
+        REFERENCES uploaders (app_id, uploader_id),
+    --
+    CONSTRAINT fk_uploaded_file_sheets_commited_by
+        FOREIGN KEY (commited_by)
+        REFERENCES users (user_id)
+);
+
