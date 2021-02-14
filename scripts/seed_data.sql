c188a7f1738f5cbe42bb3b861031934d1d500f4a
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-14 14:49:41 +0100

    Navigation extras (env_name, calendar)

--- scripts/seed_data.sql
+++ scripts/seed_data.sql
@@ -1,4 +1,12 @@
 --
+-- CREATE APP
+--
+INSERT INTO apps (app_id, is_active) VALUES (700, 'Y');
+COMMIT;
+
+
+
+--
 -- LOGIN
 --
 EXEC recompile;
@@ -26,25 +34,41 @@ COMMIT;
 DECLARE
     in_app_id           CONSTANT navigation.app_id%TYPE := sess.get_app_id();
 BEGIN
-    --
-    -- CLEANUP
-    --
-    DELETE FROM navigation WHERE app_id = in_app_id;
-    --
-    -- SHARE BUTTON
-    --
-    INSERT INTO navigation (app_id, page_id, order#, css_class)
-    SELECT in_app_id, -1, 665, 'TRANSPARENT' FROM DUAL;
+    DELETE FROM navigation
+    WHERE app_id = in_app_id;
     --
     -- LEFT/RIGHT splitter
     --
     INSERT INTO navigation (app_id, page_id, order#, css_class)
-    SELECT in_app_id, 0, 666, 'hidden' FROM DUAL;        
+    SELECT in_app_id, 0, 666, 'HIDDEN' FROM DUAL;
     --
     -- USER LOGIN/LOGOUT
     --
     INSERT INTO navigation (app_id, page_id, order#)
     SELECT in_app_id, 9999, 999 FROM DUAL;
+    --
+    COMMIT;
+END;
+/
+DECLARE
+    in_app_id           CONSTANT navigation.app_id%TYPE := sess.get_app_id();
+BEGIN
+    DELETE FROM navigation_extras
+    WHERE app_id = in_app_id;
+    --
+    -- ENV_NAME
+    --
+    INSERT INTO navigation_extras (app_id, page_alias, page_name, page_title, page_target, page_onclick, order#, css_class)
+    VALUES (in_app_id, 'ENV_NAME', '${ENV_NAME}', '', '#', 'return false;', 1, 'ENV_NAME');
+    --
+    -- SHARE BUTTON
+    --
+    NULL;
+    --
+    -- CALENDAR
+    --
+    INSERT INTO navigation_extras (app_id, page_alias, page_name, order#, css_class)
+    VALUES (in_app_id, 'CALENDAR', '<input value="${DATE}" id="DATE_PICKER" class="DATE_PICKER" />', 851, 'CALENDAR');
     --
     COMMIT;
 END;
