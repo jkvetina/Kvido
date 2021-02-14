12149b0d1fa2191f3addd3c803e06fb6ae3d471d
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-14 19:08:36 +0100

    Targets for uploaded Sheets

---
+++ views/p850_uploaded_targets.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE VIEW p850_uploaded_targets AS
+WITH s AS (
+    SELECT s.*
+    FROM uploaded_file_sheets s
+    WHERE s.file_name   = apex.get_item('$FILE')
+        AND s.sheet_id  = apex.get_item('$SHEET')
+)
+SELECT
+    CASE WHEN u.uploader_id = apex.get_item('$TARGET')
+        THEN '<b>' || NVL(u.uploader_id, '-') || '</b>'
+        ELSE          NVL(u.uploader_id, '-')
+        END AS list_label,
+    --
+    '[' || u.page_id || '] ' || u.page_name || ' - ' || u.region_name AS supplemental,
+    --
+    apex.get_page_link (
+        in_page_id      => sess.get_page_id(),
+        in_names        => 'P850_FILE,P850_SHEET,P850_TARGET',
+        in_values       => s.file_name || ',' || s.sheet_id || ',' || u.uploader_id
+    ) AS target_url
+FROM p860_uploaders_possible u
+CROSS JOIN s
+WHERE u.is_active = 'Y';
+
