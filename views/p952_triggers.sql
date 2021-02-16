6936a73e2d15bcc2ad02084de07e633787184cd1
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-16 21:31:52 +0100

    Show affected rows in Triggers

--- views/p952_triggers.sql
+++ views/p952_triggers.sql
@@ -1,6 +1,34 @@
 CREATE OR REPLACE VIEW p952_triggers AS
+WITH r AS (
+    SELECT
+        l.module_name,
+        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"INSERTED:(\d+)"', 1, 1, NULL, 1))) AS inserted_,
+        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"UPDATED:(\d+)"',  1, 1, NULL, 1))) AS updated_,
+        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"DELETED:(\d+)"',  1, 1, NULL, 1))) AS deleted_
+    FROM logs l
+    WHERE l.app_id          = sess.get_app_id()
+        AND l.created_at    >= app.get_date()
+        AND l.created_at    <  app.get_date() + 1
+        AND l.flag          = 'R'
+        AND l.action_name   = 'TRIGGER'
+        AND l.arguments     IS NOT NULL
+    GROUP BY l.module_name
+),
+m AS (
+    SELECT
+        l.module_name,
+        COUNT(l.log_id)     AS calls_
+    FROM logs l
+    WHERE l.app_id          = sess.get_app_id()
+        AND l.created_at    >= app.get_date()
+        AND l.created_at    <  app.get_date() + 1
+        AND l.flag          = 'M'
+        AND l.action_name   = 'TRIGGER'
+    GROUP BY l.module_name
+)
 SELECT
     t.table_name,
+    g.trigger_name,
     --
     CASE
         WHEN g.trigger_name = t.table_name || '__'  -- get_trigger_name()
@@ -18,17 +46,21 @@ SELECT
         THEN apex.get_icon('fa-check-square', '')
         END AS trigger_valid,
     --
-    0 AS inserted_,
-    0 AS updated_,
-    0 AS deleted_,
+    m.calls_,
+    r.inserted_,
+    r.updated_,
+    r.deleted_,
     0 AS error_,
-    0 AS not_mapped_,
-    0 AS uploaded_
+    0 AS not_mapped_
 FROM user_tables t
 LEFT JOIN user_triggers g
     ON g.table_name     = t.table_name
-LEFT JOIN user_mviews m
-    ON m.mview_name     = t.table_name
+LEFT JOIN user_mviews v
+    ON v.mview_name     = t.table_name
+LEFT JOIN m
+    ON m.module_name    = g.trigger_name
+LEFT JOIN r
+    ON r.module_name    = g.trigger_name
 WHERE t.table_name      NOT LIKE '%\_%$' ESCAPE '\'
     AND t.table_name    NOT IN (
         'LOGS',
@@ -36,5 +68,5 @@ WHERE t.table_name      NOT LIKE '%\_%$' ESCAPE '\'
         'LOGS_SETUP',
         'SESSIONS'
     )
-    AND m.mview_name    IS NULL;
+    AND v.mview_name    IS NULL;
 
