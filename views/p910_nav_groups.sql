f334b0ccce8aced5649c9efe01d28bfb5b624822
Jan Kvetina <jan.kvetina@gmail.com>
2021-02-20 15:43:09 +0100

    Removed grouping, but ROWID still not usable in Grid

--- views/p910_nav_groups.sql
+++ views/p910_nav_groups.sql
@@ -4,24 +4,33 @@ SELECT
     g.page_id,
     g.page_group,
     --
-    CASE WHEN MAX(s.procedure_name) IS NOT NULL
+    CASE WHEN s.procedure_name IS NOT NULL
         THEN apex.get_icon('fa-check-square', 'Auth procedure exists')
         END AS status,
     --
-    NULLIF(COUNT(p.page_id), 0)         AS pages,
-    MAX(a.authorization_scheme_name)    AS auth_scheme,
+    NULLIF(p.pages, 0)              AS pages,
+    a.authorization_scheme_name     AS auth_scheme
     --
-    MAX(g.ROWID) AS rid
+    --g.ROWID AS rid
 FROM navigation_groups g
-LEFT JOIN apex_application_pages p
-    ON p.application_id                 = g.app_id
-    AND p.page_group                    = g.page_group
+JOIN apex_application_pages c
+    ON c.application_id                 = g.app_id
+    AND c.page_id                       = g.page_id
+LEFT JOIN (
+    SELECT
+        p.page_group,
+        COUNT(*) AS pages
+    FROM apex_application_pages p
+    WHERE p.application_id              = sess.get_app_id()
+        AND p.page_group                IS NOT NULL
+    GROUP BY p.page_group
+) p
+    ON p.page_group                     = g.page_group
 LEFT JOIN apex_application_authorization a
-    ON a.application_id                 = p.application_id
-    AND a.authorization_scheme_name     = p.authorization_scheme
+    ON a.application_id                 = c.application_id
+    AND a.authorization_scheme_name     = c.authorization_scheme
 LEFT JOIN user_procedures s
     ON s.object_name                    = 'AUTH'  -- nav.auth_package
-    AND s.procedure_name                = p.authorization_scheme
-WHERE g.app_id                          = sess.get_app_id()
-GROUP BY g.app_id, g.page_id, g.page_group;
+    AND s.procedure_name                = c.authorization_scheme
+WHERE g.app_id                          = sess.get_app_id();
 
