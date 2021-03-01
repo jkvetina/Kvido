bbb51b6f4c5dc3ee773322e5fc1ec0e9562376cf
Jan Kvetina <jan.kvetina@gmail.com>
2021-03-01 22:08:10 +0100

    Fixed Triggers for G flags, added Uploaders check

--- views/p952_triggers.sql
+++ views/p952_triggers.sql
@@ -2,69 +2,56 @@ CREATE OR REPLACE FORCE VIEW p952_triggers AS
 WITH r AS (
     SELECT
         l.module_name,
-        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"INSERTED:(\d+)"', 1, 1, NULL, 1))) AS inserted_,
-        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"UPDATED:(\d+)"',  1, 1, NULL, 1))) AS updated_,
-        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"DELETED:(\d+)"',  1, 1, NULL, 1))) AS deleted_
+        COUNT(l.log_id)                                                                     AS calls_,
+        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"INSERTED:(\d+)"', 1, 1, NULL, 1)))   AS inserted_,
+        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"UPDATED:(\d+)"',  1, 1, NULL, 1)))   AS updated_,
+        SUM(TO_NUMBER(REGEXP_SUBSTR(l.arguments, '^[[]"DELETED:(\d+)"',  1, 1, NULL, 1)))   AS deleted_
     FROM logs l
     WHERE l.app_id          = sess.get_app_id()
         AND l.created_at    >= app.get_date()
         AND l.created_at    <  app.get_date() + 1
-        AND l.flag          = 'R'
-        AND l.action_name   = 'TRIGGER'
+        AND l.flag          = 'G'
         AND l.module_name   = CASE WHEN apex.get_item('$TABLE') IS NOT NULL THEN apex.get_item('$TABLE') || '__' ELSE l.module_name END
         AND l.arguments     IS NOT NULL
-    GROUP BY l.module_name
-),
-m AS (
-    SELECT
-        l.module_name,
-        COUNT(l.log_id)     AS calls_
-    FROM logs l
-    WHERE l.app_id          = sess.get_app_id()
-        AND l.created_at    >= app.get_date()
-        AND l.created_at    <  app.get_date() + 1
-        AND l.flag          = 'M'
-        AND l.action_name   = 'TRIGGER'
-        AND l.module_name   = CASE WHEN apex.get_item('$TABLE') IS NOT NULL THEN apex.get_item('$TABLE') || '__' ELSE l.module_name END
     GROUP BY l.module_name
 )
 SELECT
     t.table_name,
     g.trigger_name,
     --
-    CASE
-        WHEN g.trigger_name = t.table_name || '__'  -- get_trigger_name()
-            AND g.trigger_type = 'COMPOUND'
-        THEN apex.get_icon('fa-check-square', '')
-        END AS trigger_present,
+    apex.get_icon('fa-database-pointer', 'Show table details') AS table_,
     --
     CASE
-        WHEN g.triggering_event     = 'INSERT OR UPDATE OR DELETE'
+        WHEN g.trigger_name         = t.table_name || '__'  -- get_trigger_name(), used in TREE package too
+            AND g.trigger_type      = 'COMPOUND'
+            AND g.triggering_event  = 'INSERT OR UPDATE OR DELETE'
             AND g.before_statement  = 'YES'
             AND g.before_row        = 'YES'
             AND g.after_row         = 'YES'
             AND g.after_statement   = 'YES'
             AND g.status            = 'ENABLED'
-        THEN apex.get_icon('fa-check-square', '')
-        END AS trigger_valid,
+        THEN apex.get_icon('fa-check-square', 'Compound trigger present and valid')
+        END AS trigger_,
     --
-    m.calls_,
+    CASE
+        WHEN u.uploader_id IS NOT NULL
+            THEN apex.get_icon('fa-check-square', 'Table has Uploader')
+            END AS uploader_,
+    --
+    r.calls_,
     r.inserted_,
     r.updated_,
-    r.deleted_,
-    0 AS error_,
-    0 AS not_mapped_,
-    --
-    apex.get_icon('fa-database', 'Show table details') AS table_
+    r.deleted_
 FROM user_tables t
 LEFT JOIN user_triggers g
     ON g.table_name     = t.table_name
 LEFT JOIN user_mviews v
     ON v.mview_name     = t.table_name
-LEFT JOIN m
-    ON m.module_name    = g.trigger_name
 LEFT JOIN r
     ON r.module_name    = g.trigger_name
+LEFT JOIN uploaders u
+    ON u.app_id         = sess.get_app_id()
+    AND u.target_table  = t.table_name
 WHERE t.table_name      NOT LIKE '%\_%$' ESCAPE '\'
     AND t.table_name    NOT IN (
         'LOGS',
