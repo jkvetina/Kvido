2ee2d773580435c5b0839c5035abd4eaa41c4fea
Jan Kvetina <jan.kvetina@gmail.com>
2021-03-13 23:17:18 +0100

    Show USERENV and CGI info in Footer (to Developers only)

--- apex/f700/application/pages/page_00000.sql
+++ apex/f700/application/pages/page_00000.sql
@@ -19,7 +19,7 @@ wwv_flow_api.create_page(
 ,p_group_id=>wwv_flow_api.id(63772249988014549)
 ,p_protection_level=>'D'
 ,p_last_updated_by=>'DEV'
-,p_last_upd_yyyymmddhh24miss=>'20210228213202'
+,p_last_upd_yyyymmddhh24miss=>'20210313193049'
 );
 wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(13050065109981133)
@@ -38,7 +38,6 @@ wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(13050107006981134)
 ,p_plug_name=>'Page Items'
 ,p_region_template_options=>'#DEFAULT#'
-,p_escape_on_http_output=>'Y'
 ,p_plug_template=>wwv_flow_api.id(64142195941700285)
 ,p_plug_display_sequence=>20
 ,p_include_in_reg_disp_sel_yn=>'Y'
@@ -52,6 +51,9 @@ wwv_flow_api.create_page_plug(
 ,p_plug_query_num_rows=>1000
 ,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
 ,p_plug_required_role=>wwv_flow_api.id(63770652250014528)
+,p_plug_footer=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'<br />',
+''))
 ,p_attribute_02=>'ITEM_NAME'
 ,p_attribute_06=>'SUPPLEMENTAL_'
 ,p_attribute_08=>'ITEM_VALUE'
@@ -60,7 +62,6 @@ wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(13050352081981136)
 ,p_plug_name=>'App Items'
 ,p_region_template_options=>'#DEFAULT#'
-,p_escape_on_http_output=>'Y'
 ,p_plug_template=>wwv_flow_api.id(64142195941700285)
 ,p_plug_display_sequence=>30
 ,p_include_in_reg_disp_sel_yn=>'Y'
@@ -103,9 +104,107 @@ wwv_flow_api.create_page_plug(
 ,p_prn_page_header_alignment=>'CENTER'
 ,p_prn_page_footer_alignment=>'CENTER'
 ,p_prn_border_color=>'#666666'
+,p_plug_footer=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'<br />',
+''))
 ,p_attribute_02=>'ITEM_NAME'
 ,p_attribute_06=>'SUPPLEMENTAL_'
 ,p_attribute_08=>'ITEM_VALUE'
+);
+wwv_flow_api.create_page_plug(
+ p_id=>wwv_flow_api.id(89992872435818405)
+,p_plug_name=>'USERENV'
+,p_region_template_options=>'#DEFAULT#'
+,p_plug_template=>wwv_flow_api.id(64142195941700285)
+,p_plug_display_sequence=>60
+,p_include_in_reg_disp_sel_yn=>'Y'
+,p_plug_grid_column_span=>6
+,p_plug_display_point=>'REGION_POSITION_05'
+,p_query_type=>'SQL'
+,p_plug_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'WITH t AS (',
+'    SELECT',
+'        ''CLIENT_IDENTIFIER,CLIENT_INFO,ACTION,MODULE,'' ||',
+'        ''CURRENT_SCHEMA,CURRENT_USER,CURRENT_EDITION_ID,CURRENT_EDITION_NAME,'' ||',
+'        ''OS_USER,POLICY_INVOKER,'' ||',
+'        ''SESSION_USER,SESSIONID,SID,SESSION_EDITION_ID,SESSION_EDITION_NAME,'' ||',
+'        ''AUTHENTICATED_IDENTITY,AUTHENTICATION_DATA,AUTHENTICATION_METHOD,IDENTIFICATION_TYPE,'' ||',
+'        ''ENTERPRISE_IDENTITY,PROXY_ENTERPRISE_IDENTITY,PROXY_USER,'' ||',
+'        ''GLOBAL_CONTEXT_MEMORY,GLOBAL_UID,'' ||',
+'        ''AUDITED_CURSORID,ENTRYID,STATEMENTID,CURRENT_SQL,CURRENT_BIND,'' ||',
+'        ''HOST,SERVER_HOST,SERVICE_NAME,IP_ADDRESS,'' ||',
+'        ''DB_DOMAIN,DB_NAME,DB_UNIQUE_NAME,DBLINK_INFO,DATABASE_ROLE,ISDBA,'' ||',
+'        ''INSTANCE,INSTANCE_NAME,NETWORK_PROTOCOL,'' ||',
+'        ''LANG,LANGUAGE,NLS_TERRITORY,NLS_CURRENCY,NLS_SORT,NLS_DATE_FORMAT,NLS_DATE_LANGUAGE,NLS_CALENDAR,'' ||',
+'        ''BG_JOB_ID,FG_JOB_ID'' AS str',
+'    FROM DUAL',
+')',
+'SELECT',
+'    t.name,',
+'    t.value',
+'FROM (',
+'    SELECT',
+'        REGEXP_SUBSTR(str, ''[^,]+'', 1, LEVEL) AS name,',
+'        SYS_CONTEXT(''USERENV'', REGEXP_SUBSTR(str, ''[^,]+'', 1, LEVEL)) AS value',
+'    FROM t',
+'    CONNECT BY LEVEL <= REGEXP_COUNT(str, '','')',
+') t',
+'WHERE t.value IS NOT NULL',
+'ORDER BY 1;',
+''))
+,p_plug_source_type=>'NATIVE_JQM_LIST_VIEW'
+,p_plug_query_num_rows=>15
+,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
+,p_plug_required_role=>wwv_flow_api.id(63770652250014528)
+,p_plug_footer=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'<br />',
+''))
+,p_attribute_01=>'SEARCH'
+,p_attribute_02=>'NAME'
+,p_attribute_08=>'VALUE'
+,p_attribute_18=>'CLIENT'
+);
+wwv_flow_api.create_page_plug(
+ p_id=>wwv_flow_api.id(89992987819818406)
+,p_plug_name=>'CGI'
+,p_region_template_options=>'#DEFAULT#'
+,p_plug_template=>wwv_flow_api.id(64142195941700285)
+,p_plug_display_sequence=>70
+,p_include_in_reg_disp_sel_yn=>'Y'
+,p_plug_new_grid_row=>false
+,p_plug_grid_column_span=>6
+,p_plug_display_point=>'REGION_POSITION_05'
+,p_query_type=>'SQL'
+,p_plug_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'WITH t AS (',
+'    SELECT',
+'        ''QUERY_STRING,AUTHORIZATION,DAD_NAME,DOC_ACCESS_PATH,DOCUMENT_TABLE,'' ||',
+'        ''HTTP_ACCEPT,HTTP_ACCEPT_ENCODING,HTTP_ACCEPT_CHARSET,HTTP_ACCEPT_LANGUAGE,'' ||',
+'        ''HTTP_COOKIE,HTTP_HOST,HTTP_PRAGMA,HTTP_REFERER,HTTP_USER_AGENT,'' ||',
+'        ''PATH_ALIAS,PATH_INFO,REMOTE_ADDR,REMOTE_HOST,REMOTE_USER,'' ||',
+'        ''REQUEST_CHARSET,REQUEST_IANA_CHARSET,REQUEST_METHOD,REQUEST_PROTOCOL,'' ||',
+'        ''SCRIPT_NAME,SCRIPT_PREFIX,SERVER_NAME,SERVER_PORT,SERVER_PROTOCOL'' AS str',
+'    FROM DUAL',
+')',
+'SELECT',
+'    REGEXP_SUBSTR(str, ''[^,]+'', 1, LEVEL) AS name,',
+'    REGEXP_REPLACE(OWA_UTIL.GET_CGI_ENV(REGEXP_SUBSTR(str, ''[^,]+'', 1, LEVEL)), ''([;)])'', ''\1',
+''') AS value',
+'FROM t',
+'CONNECT BY LEVEL <= REGEXP_COUNT(str, '','')',
+'ORDER BY 1;',
+''))
+,p_plug_source_type=>'NATIVE_JQM_LIST_VIEW'
+,p_plug_query_num_rows=>15
+,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
+,p_plug_required_role=>wwv_flow_api.id(63770652250014528)
+,p_plug_footer=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'<br />',
+''))
+,p_attribute_01=>'ADVANCED_FORMATTING:SEARCH'
+,p_attribute_05=>'&NAME.'
+,p_attribute_08=>'VALUE'
+,p_attribute_18=>'CLIENT'
 );
 wwv_flow_api.create_page_da_event(
  p_id=>wwv_flow_api.id(13050561578981138)
