4135fb2df3bdef3c106dd94d8f67db795836a02a
Jan Kvetina <jan.kvetina@gmail.com>
2021-05-03 18:43:33 +0200

    Use roles for apps, at least 1 role is required to login

--- procedures/sess_create_user.sql
+++ procedures/sess_create_user.sql
@@ -1,21 +1,42 @@
 CREATE OR REPLACE PROCEDURE sess_create_user (
-    in_username         users.user_id%TYPE
+    in_user_login       users.user_id%TYPE,
+    in_user_id          users.user_id%TYPE,
+    in_user_name        users.user_name%TYPE
 ) AS
+    PRAGMA AUTONOMOUS_TRANSACTION;
 BEGIN
-    tree.log_module(in_username);
+    tree.log_module(in_user_login, in_user_id, in_user_name);
     --
-    INSERT INTO users (user_id, user_login, is_active, updated_by, updated_at)
-    VALUES (
-        in_username,
-        in_username,
-        'Y',
-        in_username,
-        SYSDATE
-    );
+    BEGIN
+        INSERT INTO users (user_id, user_login, is_active, updated_by, updated_at)
+        VALUES (
+            in_user_id,
+            in_user_login,
+            'Y',
+            in_user_name,
+            SYSDATE
+        );
+    EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+        NULL;
+    END;
+    --
+    BEGIN
+        INSERT INTO user_roles (user_id, role_id, is_active, updated_by, updated_at)
+        VALUES (
+            in_user_id,
+            'USER',
+            'Y',
+            in_user_name,
+            SYSDATE
+        );
+    EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+        NULL;
+    END;
+    --
+    COMMIT;
     --
     tree.update_timer();
-EXCEPTION
-WHEN DUP_VAL_ON_INDEX THEN
-    NULL;
 END;
 /
